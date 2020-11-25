1e5b3ece1c26587e284ffc3d69bb717ff1e652d6
inductionotg <ritesh2000.sinha@gmail.com>
2020-11-25 07:43:49 +0530

    Apply makemigrations and migrate

--- crud/testApp/models.py
+++ crud/testApp/models.py
@@ -2,8 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Employee(models.Model):
-    eno=model.IntegerField()
+    eno=models.IntegerField()
     ename=models.CharField(max_length=64)
     esal=models.FloatField()
     eaddr=models.CharField(max_length=256)
-    
