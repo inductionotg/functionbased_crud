9fe39c9f1a2a07f403eaeec19440a9f9cc55caae
inductionotg <ritesh2000.sinha@gmail.com>
2020-11-25 07:50:42 +0530

    Configure Admin.py for model.py

--- crud/testApp/admin.py
+++ crud/testApp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from testApp.models import Employee
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display=['eno','ename','esal','eaddr']
+admin.site.register(Employee,EmployeeAdmin)
