7549414d1ff418d8250315036fd65cff9316908f
inductionotg <ritesh2000.sinha@gmail.com>
2020-11-25 08:36:30 +0530

    Add retrieve function

--- crud/testApp/views.py
+++ crud/testApp/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from testApp.models import Employee
 # Create your views here.
+def ret_view(request):
+    employee=Employee.objects.all()
+    return render(request,'testApp/index.html',{'employee':employee})
