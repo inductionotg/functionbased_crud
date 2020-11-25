35de21a6e425064daeb18b247cefc1fb490d02ca
inductionotg <ritesh2000.sinha@gmail.com>
2020-11-25 09:15:39 +0530

    Define a create_view fucntion for creating a form to display newrecord

--- crud/testApp/views.py
+++ crud/testApp/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from testApp.models import Employee
+from testApp.forms import EmployeeForm
 # Create your views here.
 def ret_view(request):
     employee=Employee.objects.all()
     return render(request,'testApp/index.html',{'employee':employee})
+
+def create_view(request):
+    form=EmployeeForm()
+    return render(request,'testApp/create.html',{'form':form})
