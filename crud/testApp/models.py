be25f784bc958d7dbeb3ce1bc47da5cb8fa7054a
inductionotg <ritesh2000.sinha@gmail.com>
2020-11-25 07:28:29 +0530

    Create a model

--- crud/testApp/models.py
+++ crud/testApp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Employee(models.Model):
+    eno=model.IntegerField()
+    ename=models.CharField(max_length=64)
+    esal=models.FloatField()
+    eaddr=models.CharField(max_length=256)
+    
